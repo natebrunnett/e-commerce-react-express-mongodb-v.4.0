2d3978fbed3c41c92315adb9358463b5e93c6305
Nathan Brunnett <natebrunnett@gmail.com>
2024-02-11 09:28:55 +0100

    added React Helmet

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -3,38 +3,49 @@
   // price: 1699 
   // quantity: 1
 
+import {Helmet} from "react-helmet";
+
 let Products = (props) => {
 
 	let renderProducts=()=>(
-    props.thisProducts.map((prod,idx)=>{
-    let thisPrice = parseFloat(prod.price * .01).toFixed(2);
-    return(
-    <div key={idx} className="product">
-    <div className="top">
-        <img src={prod.image[0]} />
-        <div className="productTitle" 
-        
-         
-        ><b>{prod.name}</b></div>
-        <p 
-        style={{ fontSize: '14px'}}
-        ><b>{thisPrice}€</b></p>
-        <p className="description" 
-        >{prod.description}</p>
-        <p>Quantity: {prod.quantity}</p>
-        {/*<p key={idx}>Idx: {idx}</p>*/}
-    </div>
-    <div className="bottom">
-        <button onClick={() => props.AddToCart({idx})}>Add to cart</button>	
-    </div>
-    </div>)})
+        props.thisProducts.map((prod,idx)=>{
+            let thisPrice = parseFloat(prod.price * .01).toFixed(2);
+            return(
+                <div key={idx} className="product">
+                    <div className="top">
+                        <img src={prod.image[0]} />
+                        <div className="productTitle" ><b>{prod.name}</b></div>
+                        <p style={{ fontSize: '14px'}}><b>{thisPrice}€</b></p>
+                        <p className="description" >{prod.description}</p>
+                        <p>Quantity: {prod.quantity}</p>
+                        {/*<p key={idx}>Idx: {idx}</p>*/}
+                    </div>
+                    <div className="bottom">
+                        <button onClick={() => props.AddToCart({idx})}>Add to cart</button>	
+                    </div>
+                </div>
+            )
+        })
     )
-	return (
-	<><h1>Dishes</h1>
-	<div className="productGrid">
+	
+    return (
+	<>
+
+    <Helmet>
+      <title>Amazing delivery app</title>
+      <meta charSet="utf-8" />
+      <meta name="description" content="Example delivery application with stripe checkout" />
+      <meta name='keywords' content='stripe checkout, example payment implementation'/>
+      <meta name='viewport' content='width=device-width, initial-scale=1'/>
+    </Helmet>
+
+    <h1>Dishes</h1>
+	
+    <div className="productGrid">
 	{renderProducts()}
 	</div>
-	</>)
+	
+    </>)
 }
 
 export default Products
---
+++ server/controllers/emails.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer')
+const dotenv = require('dotenv');
+dotenv.config();
+const senderEmail = process.env.NODEMAILER_EMAIL;
+const senderPassword = process.env.NODEMAILER_PASSWORD;
+
+const transport = nodemailer.createTransport({
+	service: 'Gmail',
+	port: 465,
+	secure: true,
+	auth: {
+		user: senderEmail,
+		pass: senderPassword,
+	},
+});
+
+const URL = process.env.DOMAIN + '/enter/';
+
+const send_magic_link = async (email,link,which) => {
+	if(which == 'signup'){
+		var subj="Your sign up link",
+		body= '<p>Hello friend and welcome to our website. This is your link to confirm your account: '+(URL+email+'/'+link)+ '</p><p>Needless to remind you not to share this link with anyone 🤫</p>'
+	}else{
+		var subj="Your sign in link",
+		body= '<p>Hello friend and welcome back. This is your link to sign in to your account: '+(URL+email+'/'+link)+ '</p><p>Needless to remind you not to share this link with anyone 🤫</p>' }
+		const mailOptions = {
+			to: email,
+			from: senderEmail,
+			subject: subj,
+			html: body
+		}
+		try{
+			const response = await transport.sendMail(mailOptions)
+			console.log('Link sent')
+			// console.log('email= ' + process.env.NODEMAILER_EMAIL)
+			// console.log('password= ' + senderPassword)
+			return({ok:true,message:'email sent'})
+		}
+		catch( err ){
+			console.log("Something didn't work out 😭", err)
+			return({ok:false,message:err})
+		}
+	}
+
+	module.exports = { send_magic_link }
