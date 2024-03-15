530252bcf5cee883bdad854df5086915885d615a
Nathan Brunnett <natebrunnett@gmail.com>
2024-03-15 21:44:25 +0100

    build complete! config.js updated

--- client/src/components/config.js
+++ client/src/components/config.js
@@ -1,3 +1,3 @@
-const URL = 'http://localhost:3030'
+const URL = 'https://fair-leotard-cod.cyclic.app'
 
 export default URL
--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from "react-router-dom";
+import { MdRestaurantMenu } from "react-icons/md";
 
 function LandingPage({setNav}) {
   useEffect(()=> {
@@ -8,9 +9,12 @@ function LandingPage({setNav}) {
   const navigate = useNavigate()
   return (
     <div className='flex flex-col w-screen h-screen bg-slate-300 items-center justify-center'>
-        <h1>E Commerce Delivery App</h1>
-        <div>Food Icon</div>
-        <button onClick={() => navigate('/Menu')}>Menu</button>
+        <h1 className='font-bold mb-3 text-3xl text-center ml-6'>E commerce delivery app
+        <span className='text-sm italic'> v-4.0</span></h1>
+        <div><MdRestaurantMenu size={"96px"}/></div>
+        <button 
+        className='bg-amber-700 pl-6 pr-6 pt-3 pb-3 mt-3 text-white rounded-3xl'
+        onClick={() => navigate('/Menu')}>Menu</button>
     </div>
   )
 }
